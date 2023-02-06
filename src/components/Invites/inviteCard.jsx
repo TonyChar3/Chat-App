d1e37ff115adaed0475980a09a35fa8a7cce7a53
TonyChar3 <tonyc6731@gmail.com>
2023-02-06 11:06:52 -0500

    Did the styling of the received invitations cards.

--- src/components/Invites/inviteCard.jsx
+++ src/components/Invites/inviteCard.jsx
@@ -1,17 +1,33 @@
 import './invitCard.css';
+import {useState} from 'react';
 
 
 const InviteCard = ({ sender_name, sender_email }) => {
+
+    const [arrow, setArrow] = useState(false);
+
+    const handleArrowClick = (e) =>{
+        e.preventDefault();
+        setArrow(arrow => !arrow);
+    }
+
     return(
         <div className="invitationCard__container">
-            <div className="senderInfo__container">
-                <select className="sender__name" name={sender_name}>
-                    <option className="sender__email" value={sender_email}>{sender_email}</option>
-                </select>
+            <div className="Info__wrapper">
+                <div className="senderInfo__container">
+                    <span className="sender__name">{sender_name}</span>
+                    <div className={`arrow_wrapper ${arrow? "" : "down"}`}>
+                        <i onClick={(e) => handleArrowClick(e)} class={`bi bi-caret-up `}></i>
+                    </div>
+                </div>
+                <div className={`email__wrapper ${arrow? "show-email" : ""}`}>
+                    <span className="sender__email">{sender_email}</span>
+                </div>
+                
             </div>
             <div className="requestBtn__container">
-                <button>Accept<i className="bi bi-check"></i></button>
-                <button>Decline<i className="bi bi-x"></i></button>
+                <button className="accept__btn"><i className="bi bi-check"></i></button>
+                <button className="decline__btn"><i className="bi bi-x"></i></button>
             </div>
         </div>
     );
