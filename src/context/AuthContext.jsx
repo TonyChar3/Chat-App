0f73a456258b0646ef510ec1a49bd98c7a64666f
TonyChar3 <tonyc6731@gmail.com>
2023-02-20 11:29:58 -0500

    Figuring out how to keep the users name and email up to date across all the database

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -31,33 +31,37 @@ export const AuthContextProvider = ({children}) => {
         return signOut(auth)
     }
 
+    const fetchContactsList = () => {
+        console.log('being called')
+        const q = query(collection(db,"users"), where("user_uid", "==", auth.currentUser.uid));
+    
+        const unsubscribe = onSnapshot(q, (querySnapshot) => {
+            
+            const contacte = [];
+            
+            querySnapshot.forEach((doc) => {
+            
+                doc.data().contact.forEach(con => {
+                    console.log(con)
+                    contacte.push(con)
+                })
+                                 
+            })
+            setContacts(contacte)         
+        })
+
+        return () => unsubscribe;
+    }
+
+
     useEffect(() => {
         auth.onAuthStateChanged(function(user){
 
             if(user){
                 setUser(auth.currentUser)
-
-                const q = query(collection(db,"users"), where("user_uid", "==", auth.currentUser.uid));
-    
-                const unsubscribe = onSnapshot(q, (querySnapshot) => {
-    
-                    const contacte = [];
-    
-                    querySnapshot.forEach((doc) => {
-    
-                        doc.data().contact.forEach(con => {
-                            contacte.push(con)
-                        })
-                         
-                    })
-                    setContacts(contacte)
-                     
-                })
-                return () => unsubscribe
             }
         })
-       
-    },[])
+    })
 
     useEffect(() => {
         console.log('second useffect')
@@ -69,51 +73,55 @@ export const AuthContextProvider = ({children}) => {
                 const q = query(collection(db,'users'), where("user_uid", "==", each.id))
             
                 const unsubscribe = onSnapshot(q, (querySnapshot) => {
+
+                    const data_updated = [];
             
                     querySnapshot.forEach((docs) => {
                 
-                        const updated_data ={
+                        const updated_data = {
                             id: each.id,
                             name: docs.data().name,
                             email: docs.data().email,
                             confirmed: each.confirmed,
                             chatroom_id: each.chatroom_id
                         };
-                        const updateUserApp = async()=> {
-
-                            if(each && updated_data){
-                    
-
-                                const updtRef = doc(db, 'users', auth.currentUser.uid)
-
-                                // remove the outdated
-                                updateDoc(updtRef, {
-                                    contact: arrayRemove(each)
-                                })
-                        
-                                // add the updated
-                                updateDoc(updtRef,{
-                                    contact: arrayUnion( updated_data )
-                                }) 
-                            }
-                        }
-                        updateUserApp();
+                        data_updated.push(updated_data)
                     })
-                    setContacts([])
-                    setUpdate([])
+                    setUpdate(data_updated)
                 })
-            
             return () => unsubscribe;
+            })
+            setContacts([])  
+        }
+        // } else if(updated_contact.length > 0){
 
-            })           
-        }   
-    },[])
+        //     const updateUserApp = async()=> {
+
+        //         if(each && updated_data){
+
+
+        //             const updtRef = doc(db, 'users', auth.currentUser.uid)
+
+        //             // remove the outdated
+        //             updateDoc(updtRef, {
+        //                 contact: arrayRemove(each)
+        //             })
+            
+        //             // add the updated
+        //             updateDoc(updtRef,{
+        //                 contact: arrayUnion(updated_data)
+        //             }) 
+        //         }
+        //     }
+            
+        // }  
+    },[contacts])
 
     console.log(updated_contact)
     console.log(contacts)
     
     return(
-        <UserContext.Provider value={{ registerUser, logOut, SignIn, user, credential, updated_contact, contacts }}>
+        <UserContext.Provider value={{ registerUser, logOut, SignIn, user, credential, updated_contact, contacts, fetchContactsList }}>
             {children}
         </UserContext.Provider>
     );
